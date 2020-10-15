18868ae57ac6064fc2bc5fb70bf9391764a5bb1e
Michael Bonfils <michael.bonfils@ovhcloud.com>
2020-10-15 14:49:11 +0200

    s3api: Optimize multipart-upload operations
    Optimize multipart-upload operations:
    - Declare MPU placeholder objects ("uploadId") as ephemeral.
    - Enrich list-multipart-upload with oio.list-mpu
    
    Topic: OpenIO backend

--- swift/common/middleware/s3api/controllers/multi_upload.py
+++ swift/common/middleware/s3api/controllers/multi_upload.py
@@ -108,6 +108,7 @@ def _get_upload_info(req, app, upload_id):
     # for the upload marker. Until we get around to fixing that, just pop
     # it off for now...
     copy_source = req.headers.pop('X-Amz-Copy-Source', None)
+    req.environ['oio.ephemeral_object'] = True
     try:
         return req.get_response(app, 'HEAD', container=container, obj=obj)
     except NoSuchKey:
@@ -123,6 +124,7 @@ def _get_upload_info(req, app, upload_id):
         # ...making sure to restore any copy-source before returning
         if copy_source is not None:
             req.headers['X-Amz-Copy-Source'] = copy_source
+        req.environ['oio.ephemeral_object'] = False
 
 
 def _make_complete_body(req, s3_etag, yielded_anything):
@@ -428,6 +430,7 @@ class UploadsController(Controller):
 
         container = req.container_name + MULTIUPLOAD_SUFFIX
         try:
+            req.environ['oio.list_mpu'] = True
             resp = req.get_response(self.app, container=container, query=query)
             objects = json.loads(resp.body)
         except NoSuchBucket:
@@ -552,6 +555,7 @@ class UploadsController(Controller):
 
         req.headers.pop('Etag', None)
         req.headers.pop('Content-Md5', None)
+        req.environ['oio.ephemeral_object'] = True
 
         req.get_response(self.app, 'PUT', seg_container, obj, body='')
 
@@ -693,7 +697,9 @@ class UploadController(Controller):
         # then it was completed and we return an error here.
         container = req.container_name + MULTIUPLOAD_SUFFIX
         obj = '%s/%s' % (req.object_name, upload_id)
+        req.environ['oio.ephemeral_object'] = True
         req.get_response(self.app, container=container, obj=obj)
+        req.environ['oio.ephemeral_object'] = False
 
         # The completed object was not found so this
         # must be a multipart upload abort.
@@ -888,6 +894,7 @@ class UploadController(Controller):
 
                 # clean up the multipart-upload record
                 obj = '%s/%s' % (req.object_name, upload_id)
+                req.environ['oio.ephemeral_object'] = True
                 try:
                     req.get_response(self.app, 'DELETE', container, obj)
                 except NoSuchKey:
