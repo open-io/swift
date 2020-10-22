fd411cf9905227a5051d3eefac14a1c26ae0c503
Florent Vennetier <florent.vennetier@ovhcloud.com>
2020-10-22 18:07:17 +0200

    Implement a Redis-based IAM middleware
    Change-Id: Ic3a79b0c42e391d46b6fc59f5c75badc96c0a839

--- oio_tests/functional/run-iam-tests.sh
+++ oio_tests/functional/run-iam-tests.sh
@@ -10,9 +10,21 @@ run_sds || exit 1
 configure_aws
 configure_s3cmd
 
-# IAM
+# IAM, with static file
 RULES_FILE="$PWD/etc/iam-rules-sample.json"
-sed -e "s#%RULES_FILE%#${RULES_FILE}#g" etc/s3-iam.cfg.in > etc/s3-iam.cfg
+sed -e "s#%IAM_RULES_CONN%#file://${RULES_FILE}#g" etc/s3-iam.cfg.in > etc/s3-iam.cfg
+run_functional_test s3-iam.cfg s3-iam.sh
+
+# IAM, with rules in a Redis database
+CONN_STR="redis://127.0.0.1:6379"
+for USER in $(jq -r --raw-output 'keys | .[]' $RULES_FILE)
+do
+  RULE="$(jq -c ".\"$USER\"" $RULES_FILE)"
+  ACCOUNT="AUTH_$(echo "$USER" | cut -d ':' -f 1)"
+  openio-admin iam set-user-policy --connection "$CONN_STR" "$ACCOUNT" "$USER" "$RULE"
+done
+
+sed -e "s#%IAM_RULES_CONN%#${CONN_STR}#g" etc/s3-iam.cfg.in > etc/s3-iam.cfg
 run_functional_test s3-iam.cfg s3-iam.sh
 
 # TODO(FVE): gridinit_cmd stop
