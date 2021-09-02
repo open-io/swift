0f2d74b8d165f3d4462120e8004b522527a0b6fc
Aymeric Ducroquetz <aymeric.ducroquetz@ovhcloud.com>
2021-09-02 16:26:28 +0200

    Allow to accept only S3 requests
    Change-Id: I8156a49a82debc8836438ebc402517b119b444b8

--- swift/common/middleware/s3api/s3api.py
+++ swift/common/middleware/s3api/s3api.py
@@ -158,7 +158,8 @@ from swift.common.middleware.s3api.exception import NotS3Request, \
     InvalidSubresource
 from swift.common.middleware.s3api.s3request import get_request_class
 from swift.common.middleware.s3api.s3response import ErrorResponse, \
-    InternalError, MethodNotAllowed, S3ResponseBase, S3NotImplemented
+    InternalError, MethodNotAllowed, S3ResponseBase, S3NotImplemented, \
+    InvalidRequest
 from swift.common.utils import get_logger, config_true_value, \
     config_positive_int_value, split_path, closing_if_possible, list_from_csv
 from swift.common.middleware.s3api.utils import Config
@@ -252,6 +253,8 @@ class S3ApiMiddleware(object):
         self.conf = Config()
 
         # Set default values if they are not configured
+        self.conf.s3_only = config_true_value(
+            wsgi_conf.get('s3_only', False))
         self.conf.allow_no_owner = config_true_value(
             wsgi_conf.get('allow_no_owner', False))
         self.conf.location = wsgi_conf.get('location', 'us-east-1')
@@ -385,7 +388,10 @@ class S3ApiMiddleware(object):
             env['s3api.bucket'] = req.container_name
             resp = self.handle_request(req)
         except NotS3Request:
-            resp = self.app
+            if self.conf.s3_only and env.get('PATH_INFO') != '/info':
+                resp = InvalidRequest(reason='Not S3 request')
+            else:
+                resp = self.app
         except InvalidSubresource as e:
             self.logger.debug(e.cause)
         except ErrorResponse as err_resp:
