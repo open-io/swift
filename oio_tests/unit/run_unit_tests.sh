62871b1c5c1515d951d56f55f8b38fe0fb364569
Thibault Person <thibault.person@ovhcloud.com>
2021-09-20 16:12:44 +0200

    CI: activate unit and functional tests in cds

--- oio_tests/unit/run_unit_tests.sh
+++ oio_tests/unit/run_unit_tests.sh
@@ -2,8 +2,9 @@
 
 set -e
 
-coverage run --source=swift,oio_tests -p $(which nosetests) -v \
+coverage run -p $(which nosetests) -v \
     --with-timer --timer-ok=100ms --timer-warning=1s \
+    --with-xunit --xunit-file=tests_report.xml \
     oio_tests/unit/controllers \
     oio_tests/unit/common/middleware/crypto \
     oio_tests/unit/common/middleware/test_copy.py:TestOioServerSideCopyMiddleware \
