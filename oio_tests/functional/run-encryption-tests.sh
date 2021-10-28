1d0b57a567320e13c3e9dc81e4b3918beb1931d1
Florent Vennetier <florent.vennetier@ovhcloud.com>
2021-10-28 11:58:17 +0200

    CI: enable encryption tests
    Change-Id: I42b436a0753198c5dd7f7952da16e75e2e5bfaac

--- oio_tests/functional/run-encryption-tests.sh
+++ oio_tests/functional/run-encryption-tests.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# TODO(FVE): merge this file with run-s3-tests.sh
-
 source oio_tests/functional/common.sh
 
 export OIO_NS="OPENIO" OIO_ACCOUNT="AUTH_demo" OIO_USER=USER-$RANDOM OIO_PATH=PATH-$RANDOM
@@ -12,8 +10,9 @@ configure_aws
 
 RET=0
 
-run_functional_test s3-versioning-encryption.cfg encryption-tests.sh
-run_functional_test s3-versioning-custom-encryption.cfg custom-encryption-tests.sh s3-versioning.sh
+run_functional_test s3-encryption.cfg.in encryption-tests.sh s3-multipart.sh s3-mpu.py
+# SSE-C tests (not implemeted yet)
+#run_functional_test s3-versioning-custom-encryption.cfg custom-encryption-tests.sh s3-versioning.sh
 
 # TODO(FVE): gridinit_cmd stop
 exit $RET
