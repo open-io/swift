95346ab804e88afb53f0a29b51673890e463ccb0
Florent Vennetier <florent.vennetier@ovhcloud.com>
2021-11-19 18:36:28 +0100

    crypto: implement SSE-C encryption mode
    Change-Id: Ia7eea2cbd9a2c5f650476088f8e8a8faa20e63b5

--- oio_tests/functional/run-encryption-tests.sh
+++ oio_tests/functional/run-encryption-tests.sh
@@ -10,9 +10,9 @@ configure_aws
 
 RET=0
 
+# Data at rest encryption tests
 run_functional_test s3-encryption.cfg.in encryption-tests.sh s3-multipart.sh s3-mpu.py
-# SSE-C tests (not implemeted yet)
-#run_functional_test s3-versioning-custom-encryption.cfg custom-encryption-tests.sh s3-versioning.sh
+# SSE-C tests
+run_functional_test s3-custom-encryption.cfg.in custom-encryption-tests.sh s3-versioning.sh
 
-# TODO(FVE): gridinit_cmd stop
 exit $RET
