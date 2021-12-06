3ec70048422f5bc8b1c3afa5909a975b4e4c2487
Aymeric Ducroquetz <aymeric.ducroquetz@ovhcloud.com>
2021-12-06 11:14:08 +0100

    s3api: Fix the reading of the limit of buckets
    Change-Id: I29e358612c41fd8eab1f8646280dfcd65130ad42

--- oio_tests/functional/buckets-listing.sh
+++ oio_tests/functional/buckets-listing.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 set -e
 
-OIO_ACCOUNT="AUTH_demo"
+export OIO_NS="${1:-OPENIO}"
+export OIO_ACCOUNT="${2:-AUTH_demo}"
+
 BUCKET_1=aaa${RANDOM}
+BUCKET_2=bbb${RANDOM}
+BUCKET_3=ccc${RANDOM}
+BUCKET_4=ddd${RANDOM}
 SUBPATH=${RANDOM}
-BUCKET_2=zzz${RANDOM}
+
 AWS="aws --endpoint-url http://localhost:5000 --no-verify-ssl"
 REDIS_CLI="redis-cli"
 
@@ -27,3 +32,26 @@ set -x
 OUT=$( ${AWS} s3 ls )
 echo ${OUT} | grep ${BUCKET_1}
 echo ${OUT} | grep ${BUCKET_2}
+
+# Change the limit of buckets per account
+sudo systemctl stop memcached | true
+
+function trap_exit {
+	echo "--------------------"
+	echo "EXIT signal trapped"
+	echo "--------------------"
+	set +e
+    openio account unset -p X-Account-Meta-Max-Buckets "${OIO_ACCOUNT}"
+    sudo systemctl start memcached | true
+}
+
+trap trap_exit EXIT
+
+NB_BUCKETS=OUT=$(${AWS} s3 ls | wc -l)
+openio account set -p X-Account-Meta-Max-Buckets=$((NB_BUCKETS + 1)) "${OIO_ACCOUNT}"
+${AWS} s3api create-bucket --bucket ${BUCKET_3}
+OUT=$(${AWS} s3api create-bucket --bucket ${BUCKET_4} 2>&1 | tail -n 1)
+echo "$OUT" | grep "TooManyBuckets"
+
+openio account unset -p X-Account-Meta-Max-Buckets "${OIO_ACCOUNT}"
+${AWS} s3api create-bucket --bucket ${BUCKET_4}
