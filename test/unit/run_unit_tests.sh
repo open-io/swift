3782703b220ae280e986404efeaefa2f0e004932
Vincent Mérat <vincent.merat@ovhcloud.com>
2021-12-15 15:40:36 +0100

    intelligent-tiering: implement middleware

--- test/unit/run_unit_tests.sh
+++ test/unit/run_unit_tests.sh
@@ -5,7 +5,8 @@ set -e
 coverage run -p $(which nosetests) -v \
     --with-timer --timer-ok=100ms --timer-warning=1s \
     --with-xunit --xunit-file=tests_report.xml \
-    test/unit/common/middleware/s3api/test_intelligent_tiering.py
+    test/unit/common/middleware/s3api/test_intelligent_tiering.py \
+    test/unit/common/middleware/test_intelligent_tiering.py
 
 # TODO: fix all failed tests and run them all
 # coverage run -p $(which nosetests) -v \
