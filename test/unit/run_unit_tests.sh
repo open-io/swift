79f661cef742d3dbea3019b58eff1f05b2281976
Florent Vennetier <florent.vennetier@ovhcloud.com>
2022-02-23 10:04:38 +0100

    CI: run more unit tests
    Some tests were disabled because we did not take time to fix them with
    the OpenIO SDS backend.
    
    Change-Id: I233f2a996a8f4d3745387552a672e38da307e43b
    Topic: OpenIO backend

--- test/unit/run_unit_tests.sh
+++ test/unit/run_unit_tests.sh
@@ -6,6 +6,7 @@ coverage run -p $(which nosetests) -v \
     --with-timer --timer-ok=100ms --timer-warning=1s \
     --with-xunit --xunit-file=tests_report.xml \
     test/unit/common/middleware/s3api/test_intelligent_tiering.py \
+    test/unit/common/middleware/s3api/test_multi_delete.py \
     test/unit/common/middleware/test_intelligent_tiering.py
 
 # TODO: fix all failed tests and run them all
