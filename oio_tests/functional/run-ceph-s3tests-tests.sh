5af10d2ed3de58f6fe4c1a7bdfa3a7e01db9cf06
Florent Vennetier <florent.vennetier@ovhcloud.com>
2022-06-22 15:45:55 +0200

    CI: parse Ceph S3 tests results
    Change-Id: I45a2b9aa0240e33016aa948f7b538f62a88691da

--- oio_tests/functional/run-ceph-s3tests-tests.sh
+++ oio_tests/functional/run-ceph-s3tests-tests.sh
@@ -11,6 +11,16 @@ configure_aws
 
 RET=0
 
+# Remove the inherited ceph-s3tests repository.
+rm -rf third_party/ceph-s3tests-reports/ceph-tests
+# Replace it by our custom one.
+ln -s ../../third_party/ceph-s3tests third_party/ceph-s3tests-reports/ceph-tests
+cd third_party/ceph-s3tests-reports
+pip install -r requirements.txt
+./bin/get_ceph_test_attributes.py
+cd ../..
+mv third_party/ceph-s3tests-reports/output/ceph-s3.out.yaml ceph-s3tests-attributes.yaml
+
 run_functional_test s3-custom-encryption.cfg.in ceph-s3tests.sh
 
 exit $RET
