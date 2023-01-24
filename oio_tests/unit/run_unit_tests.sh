b8f36197b9c975668396a7377ec44236e19ba41a
Florent Vennetier <florent.vennetier@ovhcloud.com>
2023-01-24 14:15:37 +0100

    CI: fix coverage link, ignore pure swift modules
    With oio-sds, we are not using swift's account, container and objets
    servers, we can remove them from our code coverage reports.
    
    Change-Id: Ie21f3b3ae870286e41424ba92caf7a49001f3c36

--- oio_tests/unit/run_unit_tests.sh
+++ oio_tests/unit/run_unit_tests.sh
@@ -2,7 +2,10 @@
 
 set -e
 
-coverage run -p $(which nosetests) -v --exe \
+coverage run \
+  --omit=swift/account/*,swift/cli/*,swift/container/*,swift/obj/* \
+  -p $(which nosetests) \
+    -v --exe \
     --with-timer --timer-ok=100ms --timer-warning=1s \
     --with-xunit --xunit-file=tests_report.xml \
     --xunit-testsuite-name=oioswift \
