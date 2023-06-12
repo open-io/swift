202241dc066114105d1a85ae7e59fcabd7bf7fdd
Florent Vennetier <florent.vennetier@ovhcloud.com>
2023-06-12 17:15:18 +0200

    CI: include more tests in coverage reports
    Change-Id: I607bd383e9604bb3315a4744aa1f846d346fcbd5

--- test/unit/run_unit_tests.sh
+++ test/unit/run_unit_tests.sh
@@ -2,9 +2,14 @@
 
 set -e
 
-coverage run --context "swift-unit" -p $(which nosetests) -v --exe \
+coverage run \
+  --omit=swift/account/*,swift/cli/*,swift/container/*,swift/obj/* \
+  --context "swift-unit" \
+  -p \
+    $(which nosetests) \
+    -v --exe \
     --with-timer --timer-ok=100ms --timer-warning=1s \
     --with-xunit --xunit-file=tests_report.xml \
     --xunit-testsuite-name=swift \
     --ignore-files test_decrypter.py \
-    test/unit
+    test/unit/
