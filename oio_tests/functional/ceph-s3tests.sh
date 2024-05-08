a7c91c14b1f3e53431e33d2459a7aa208ed7220b
Thibault Person <thibault.person@ovhcloud.com>
2024-05-08 15:03:10 +0200

    tests: Use only one source of options for ceph tests

--- oio_tests/functional/ceph-s3tests.sh
+++ oio_tests/functional/ceph-s3tests.sh
@@ -3,14 +3,6 @@
 cd third_party/ceph-s3tests || exit 1
 ln -sTf ../../ceph-s3tests.conf ceph-s3tests.conf
 
-cat > setup.cfg <<SETUP
-[nosetests]
-with-xunit=1
-failure-detail=1
-verbosity=1
-logging-level=INFO
-SETUP
-
 # Wait for the Swift gateway to settle
 sleep 3
 # Debug: try to connect, wait 5s, diconnect immediatly after connection is established
@@ -20,6 +12,7 @@ openio account set AUTH_demo --max-buckets 1000
 
 S3TEST_CONF=ceph-s3tests.conf nosetests \
   -a '!fails_on_aws' -v \
+  -logging-level=INFO -logging-level \
   --with-xunit --xunit-file=tests_report.xml \
   s3tests_boto3.functional.test_s3 s3tests_boto3.functional.test_s3_cross_account_acl
 
