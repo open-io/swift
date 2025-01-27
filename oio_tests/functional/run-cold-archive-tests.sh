4ca3c6576a6ff020f721cf8b0321e136fa9d03ce
Vincent Mérat <vincent.merat@ovhcloud.com>
2025-01-27 18:09:40 +0100

    ci: add basic test to cold-archive pipeline

--- oio_tests/functional/run-cold-archive-tests.sh
+++ oio_tests/functional/run-cold-archive-tests.sh
@@ -15,6 +15,10 @@ RET=0
 # Intelligent Tiering with IAM in the pipeline
 RULES_FILE="$PWD/etc/iam-rules-sample.json"
 sed -e "s#%IAM_RULES_CONN%#file://${RULES_FILE}#g" etc/s3-cold-archive.cfg.in > etc/s3-cold-archive.cfg
+
+# Basic tests with the default cold archive configuration
+run_functional_test etc/s3-cold-archive.cfg s3-basic-test.py
+
 # Remove in order to test the functionality to allow/deny from conf, allow get operation only when bucket is restored
 sed -i "s#it_iam_get_object_actions = None, Restored#it_iam_get_object_actions = Restored#g" etc/s3-cold-archive.cfg
 # Allow to delete objects via Swift in deleting bucket state
