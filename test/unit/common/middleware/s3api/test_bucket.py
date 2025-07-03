2f07f7013ee175c56172757c90126ee3f874cae3
Franco Davy Irakoze <franco-davy.irakoze@ovhcloud.com>
2025-07-03 16:27:24 +0200

    test: remove support of PY2 in test s3api bucket

--- test/unit/common/middleware/s3api/test_bucket.py
+++ test/unit/common/middleware/s3api/test_bucket.py
@@ -16,7 +16,6 @@
 import unittest
 import mock
 
-import six
 from six.moves.urllib.parse import quote, quote_plus, parse_qsl
 
 from swift.common import swob
@@ -207,8 +206,7 @@ class TestS3ApiBucket(S3ApiTestCase):
             self.assertEqual('2011-01-05T02:19:14.275Z',
                              o.find('./LastModified').text)
         expected = [
-            (i[0].encode('utf-8') if six.PY2 else i[0],
-             PFS_ETAG if i[0] == 'pfs-obj' else
+            (i[0], PFS_ETAG if i[0] == 'pfs-obj' else
              '"0-N"' if i[0] == 'slo' else '"0"')
             for i in self.objects
         ]
@@ -265,8 +263,7 @@ class TestS3ApiBucket(S3ApiTestCase):
                     restore_status.find(
                         "./IsRestoreInProgress").text, 'true')
         expected = [
-            (i[0].encode('utf-8') if six.PY2 else i[0],
-             PFS_ETAG if i[0] == 'pfs-obj' else
+            (i[0], PFS_ETAG if i[0] == 'pfs-obj' else
              '"0-N"' if i[0] == 'slo' else '"0"')
             for i in self.objects
         ]
@@ -345,12 +342,8 @@ class TestS3ApiBucket(S3ApiTestCase):
         status, headers, body = self.call_s3api(req)
         elem = fromstring(body, 'ListBucketResult')
         self.assertEqual(elem.find('./IsTruncated').text, 'true')
-        if six.PY2:
-            self.assertEqual(elem.find('./NextMarker').text,
-                             u'but-\u062a/'.encode('utf-8'))
-        else:
-            self.assertEqual(elem.find('./NextMarker').text,
-                             u'but-\u062a/')
+        self.assertEqual(elem.find('./NextMarker').text,
+                         u'but-\u062a/')
 
     def test_bucket_GET_is_truncated_url_encoded(self):
         bucket_name = 'junk'
@@ -724,10 +717,7 @@ class TestS3ApiBucket(S3ApiTestCase):
         self.assertEqual(elem.findall('./DeleteMarker'), [])
         versions = elem.findall('./Version')
         objects = list(self.objects)
-        if six.PY2:
-            expected = [v[0].encode('utf-8') for v in objects]
-        else:
-            expected = [v[0] for v in objects]
+        expected = [v[0] for v in objects]
         self.assertEqual([v.find('./Key').text for v in versions], expected)
         self.assertEqual([v.find('./IsLatest').text for v in versions],
                          ['true' for v in objects])
@@ -790,8 +780,6 @@ class TestS3ApiBucket(S3ApiTestCase):
         expected = []
         for o in self.objects_list:
             name = o['name']
-            if six.PY2:
-                name = name.encode('utf8')
             expected.append((name, 'true', 'null'))
             if name == 'rose':
                 expected.append((name, 'false', '1'))
@@ -820,8 +808,6 @@ class TestS3ApiBucket(S3ApiTestCase):
         expected = []
         for o in self.objects_list[:5]:
             name = o['name']
-            if six.PY2:
-                name = name.encode('utf8')
             expected.append((name, 'true', 'null'))
             if name == 'rose':
                 expected.append((name, 'false', '1'))
@@ -998,11 +984,10 @@ class TestS3ApiBucket(S3ApiTestCase):
             ('with space', 'true', 'null'),
             ('with%20space', 'true', 'null'),
         ]
-        if not six.PY2:
-            item = list(expected[1])
-            item[0] = item[0].decode('utf8')
-            expected[1] = tuple(item)
 
+        item = list(expected[1])
+        item[0] = item[0].decode('utf8')
+        expected[1] = tuple(item)
         discovered = [
             tuple(e.find('./%s' % key).text for key in (
                 'Key', 'IsLatest', 'VersionId'))
